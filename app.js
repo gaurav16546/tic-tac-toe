f800ad3b4147534087c2f57386a67bd9b6f45980
Gaurav Thapa <gaurav16547@gmail.com>
2023-05-29 21:31:19 +0545

    no change

--- app.js
+++ app.js
@@ -24,35 +24,54 @@ const Gameboard = (function () {
         }
     }
 
-    // const checkGameCondition = (row, column, player) => {
-    //     let availableCells = 0;
-    //     for (let i = 0; i < 3; i += 1) {
-    //         for (let j = 0; j < 3; j += 1) {
-    //             if (board[i][j].getValue() === 0)
-    //                 availableCells++;
-    //         }
-    //     }
-    //     if (!availableCells) return "draw";
+    const checkGameCondition = (row, column, player) => {
+        let availableCells = 0;
+        for (let i = 0; i < 3; i += 1) {
+            for (let j = 0; j < 3; j += 1) {
+                if (board[i][j].getValue() === 'null')
+                    availableCells++;
+            }
+        }
 
-    //     if (availableCells <= 4 && availableCells > 1) {
-    //         ;
-    //         let countRow = 0;
-    //         let countColumn = 0;
-    //         let diagonalCount = 0;
-    //         for (let i = 0; i < 3; i++) {
-    //             if (board[i][column].getValue() === player.returnToken()) {
-    //                 countColumn++;
-    //             }
-    //             else if (board[row][i].getValue() === player.returnToken()) {
-    //                 countRow++;
-    //             }
-    //         }
-    //         if (board[0][0] === board[1][1] === board[2][2] || board[0][2] === board[1][1] === board[2][0]) {
-    //             diagonalCount++;
-    //         }
-    //     }
+        if (availableCells === 0) {
+            return;
+        }
 
-    // }
+        if (availableCells <= 4 && availableCells > 1) {
+            const cellArray = [];
+            for (let i = 0; i < 3; i++) {
+                if (board[i][column].getValue() === player.returnToken()) {
+                    cellArray.push([i, column]);
+                }
+            }
+            if (cellArray.length === 3) {
+                return cellArray;
+            }
+            cellArray.length = 0;
+            for (let i = 0; i < 3; i++) {
+                if (board[row][i].getValue() === player.returnToken()) {
+                    cellArray.push([row, i]);
+                }
+            }
+            if (cellArray.length === 3) {
+                return cellArray;
+            }
+            cellArray.length = 0;
+            if (board[0][0].getValue() === board[1][1].getValue() === board[2][2].getValue()) {
+                cellArray.push([0, 0], [1, 1], [2, 2]);
+                return cellArray;
+            }
+            if (board[0][2].getValue() === board[1][1].getValue() === board[2][0].getValue()) {
+                cellArray.push([0, 2], [1, 1], [2, 0]);
+                return cellArray;
+            }
+
+            cellArray.length = 0;
+            return cellArray;
+        }
+        return true;
+
+    }
     const dropToken = (row, column, player) => {
         if (board[row][column].getValue() === "null") {
             board[row][column].addToken(player);
@@ -74,7 +93,7 @@ const Gameboard = (function () {
     return {
         dropToken,
         printBoard,
-        // checkGameCondition
+        checkGameCondition
     }
 })();
 
@@ -83,11 +102,30 @@ const displayController = (function () {
     const playRound = () => {
         const r = prompt("Enter the row");
         const c = prompt("Enter the column");
-        if (Gameboard.dropToken(r, c, playableCharacter.returnToken())) {
+        if (Gameboard.dropToken(r, c, playableCharacter.returnToken()) && checkFurtherGameplay(r, c, playableCharacter)) {
             playableCharacter.switchPlayer();
             Gameboard.printBoard();
         }
     }
+    const checkFurtherGameplay = (row, column, player) => {
+        let gameCondition = Gameboard.checkGameCondition(row, column, player);
+        if (!gameCondition) {
+            console.log("Game is tied");
+            Gameboard.printBoard();
+            return false;
+        } else if (gameCondition.length === 0) {
+            return true;
+        }
+        else if (gameCondition.length === 3) {
+            playableCharacter.winMessage();
+            console.log(`Winning cell are ${gameCondition}`);
+            Gameboard.printBoard();
+            return false;
+        } else {
+            return true;
+        }
+    }
+
     return {
         playRound
     }
@@ -109,8 +147,12 @@ function Player(playerOne = "PlayerOne", playerTwo = "PlayerTwo") {
         playerTurn = playerTurn === players[0] ? players[1] : players[0];
         console.log(`${playerTurn.name}'s turn`);
     }
+    const winMessage = () => {
+        console.log(`Game won by ${playerTurn.name}`);
+    }
     const returnToken = () => playerTurn.token;
     return {
-        switchPlayer, returnToken
+        switchPlayer, returnToken, winMessage
     }
 }
+
