5066f9743e9be642554b8f716d8ffeba7e7ab1e8
Gaurav Thapa <gaurav16547@gmail.com>
2023-05-31 08:08:02 +0545

    added logic to determine the winner or tie

--- app.js
+++ app.js
@@ -11,6 +11,28 @@ function Cell() {
         getValue,
     };
 }
+function Player(playerOne = "PlayerOne", playerTwo = "PlayerTwo") {
+    const players = [
+        {
+            name: playerOne,
+            token: "X"
+        },
+        {
+            name: playerTwo,
+            token: "O"
+        }
+    ]
+    let playerTurn = players[0];
+    const switchPlayer = () => {
+        playerTurn = playerTurn === players[0] ? players[1] : players[0];
+        console.log(`${playerTurn.name}'s turn`);
+    }
+    const returnToken = () => playerTurn.token;
+    const returnName = () => playerTurn.name;
+    return {
+        switchPlayer, returnToken, returnName
+    }
+}
 
 const Gameboard = (function () {
     const row = 3;
@@ -24,8 +46,67 @@ const Gameboard = (function () {
         }
     }
 
+
+    const dropToken = (row, column, player) => {
+        if (board[row][column].getValue() === "null") {
+            board[row][column].addToken(player);
+            console.log(`Token ${player} has been added to row ${row} and column ${column}`)
+
+            return true;
+        } else {
+            console.log(`${row} by ${column} is already taken`);
+            return false;
+        }
+        // checkGameCondition(row, column, player);
+    };
+
+
+    const printBoard = () => {
+        const boardPrint = board.map((row) => row.map((cell) => cell.getValue()));
+        console.log(boardPrint);
+    };
+
+    return {
+        dropToken,
+        printBoard,
+        board
+    }
+})();
+
+const displayController = (function () {
+    const playableCharacter = Player();
+    const playRound = () => {
+        const r = prompt("Enter the row");
+        const c = prompt("Enter the column");
+        if (Gameboard.dropToken(r, c, playableCharacter.returnToken()) && checkFurtherGameplay(r, c, playableCharacter)) {
+            playableCharacter.switchPlayer();
+            Gameboard.printBoard();
+        }
+    }
+    const checkFurtherGameplay = (row, column, player) => {
+        let gameCondition = checkGameCondition(row, column, player);
+        if (!gameCondition) {
+            console.log("Game is tied");
+            Gameboard.printBoard();
+            return false;
+        } else if (gameCondition.length === 0) {
+            return true;
+        }
+        else if (gameCondition.length === 3) {
+            winMessage();
+            console.log(`Winning cell are ${gameCondition}`);
+            Gameboard.printBoard();
+            return false;
+        } else {
+            return true;
+        }
+    }
+    const winMessage = () => {
+        console.log(`Game won by ${playableCharacter.returnName()}`);
+    }
     const checkGameCondition = (row, column, player) => {
         let availableCells = 0;
+        const board = Gameboard.board;
         for (let i = 0; i < 3; i += 1) {
             for (let j = 0; j < 3; j += 1) {
                 if (board[i][j].getValue() === 'null')
@@ -57,11 +138,11 @@ const Gameboard = (function () {
                 return cellArray;
             }
             cellArray.length = 0;
-            if (board[0][0].getValue() === board[1][1].getValue() === board[2][2].getValue()) {
+            if (board[0][0].getValue() === board[1][1].getValue() && board[1][1].getValue() === board[2][2].getValue()) {
                 cellArray.push([0, 0], [1, 1], [2, 2]);
                 return cellArray;
             }
-            if (board[0][2].getValue() === board[1][1].getValue() === board[2][0].getValue()) {
+            if (board[0][2].getValue() === board[1][1].getValue() && board[1][1].getValue() === board[2][0].getValue()) {
                 cellArray.push([0, 2], [1, 1], [2, 0]);
                 return cellArray;
             }
@@ -72,87 +153,16 @@ const Gameboard = (function () {
         return true;
 
     }
-    const dropToken = (row, column, player) => {
-        if (board[row][column].getValue() === "null") {
-            board[row][column].addToken(player);
-            console.log(`Token ${player} has been added to row ${row} and column ${column}`)
-            return true;
-        } else {
-            console.log(`${row} by ${column} is already taken`);
-            return false;
-        }
-        // checkGameCondition(row, column, player);
-    };
-
-
-    const printBoard = () => {
-        const boardPrint = board.map((row) => row.map((cell) => cell.getValue()));
-        console.log(boardPrint);
-    };
-
-    return {
-        dropToken,
-        printBoard,
-        checkGameCondition
-    }
-})();
-
-const displayController = (function () {
-    const playableCharacter = Player();
-    const playRound = () => {
-        const r = prompt("Enter the row");
-        const c = prompt("Enter the column");
-        if (Gameboard.dropToken(r, c, playableCharacter.returnToken()) && checkFurtherGameplay(r, c, playableCharacter)) {
-            playableCharacter.switchPlayer();
-            Gameboard.printBoard();
-        }
-    }
-    const checkFurtherGameplay = (row, column, player) => {
-        let gameCondition = Gameboard.checkGameCondition(row, column, player);
-        if (!gameCondition) {
-            console.log("Game is tied");
-            Gameboard.printBoard();
-            return false;
-        } else if (gameCondition.length === 0) {
-            return true;
-        }
-        else if (gameCondition.length === 3) {
-            playableCharacter.winMessage();
-            console.log(`Winning cell are ${gameCondition}`);
-            Gameboard.printBoard();
-            return false;
-        } else {
-            return true;
-        }
-    }
-
     return {
         playRound
     }
 })();
 
-function Player(playerOne = "PlayerOne", playerTwo = "PlayerTwo") {
-    const players = [
-        {
-            name: playerOne,
-            token: "X"
-        },
-        {
-            name: playerTwo,
-            token: "O"
-        }
-    ]
-    let playerTurn = players[0];
-    const switchPlayer = () => {
-        playerTurn = playerTurn === players[0] ? players[1] : players[0];
-        console.log(`${playerTurn.name}'s turn`);
-    }
-    const winMessage = () => {
-        console.log(`Game won by ${playerTurn.name}`);
-    }
-    const returnToken = () => playerTurn.token;
-    return {
-        switchPlayer, returnToken, winMessage
-    }
+function screenController() {
 }
+
+
+
+
+
 
